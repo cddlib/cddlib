7d73e83b0be1297aa7ba8c767de3d1c30bcc4f81
Julian Rüth <julian.rueth@fsfe.org>
2018-05-11 17:07:31 +0200

    Added cddexec.c
    written by Komei Fukuda

--- src/cddexec.c
+++ src/cddexec.c
@@ -1,24 +1,22 @@
-/* cdd_both_reps.c: compute reduced H and V representation of polytope
-   by Volker Braun <vbraun@stp.dias.ie>
+/* cddexec.c: executing cdd functions.  It mimics the interface of the program
+   cdd_both_reps by Volker Braun <vbraun@stp.dias.ie> with much faster executions.
    
    The input is taken from stdin and can be either a 
-   H or V representation, not necessarily reduced.
-
-   based on testcdd1.c, redcheck.c, and of course the cdd library
-   written by Komei Fukuda, fukuda@ifor.math.ethz.ch
-   Standard ftp site: ftp.ifor.math.ethz.ch, Directory: pub/fukuda/cdd
+   H or V representation.
+ 
+   Written by Komei Fukuda <fukuda@math.ethz.ch>, by using some part of cdd_both_reps.
+   Version 0.94j, May 10, 2018.
+ 
 */
 
 /*  This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -31,9 +29,6 @@
 #include <time.h>
 #include <math.h>
 #include <string.h>
-
-
-
 
 
 void compute_adjacency(dd_MatrixPtr Rep, dd_ErrorType* err_ptr)
@@ -73,90 +68,134 @@ void compute_adjacency(dd_MatrixPtr Rep, dd_ErrorType* err_ptr)
 }
 
 
-void minimal_Vrep_Hrep(dd_MatrixPtr M, 
-		       dd_MatrixPtr* Vrep_ptr, dd_MatrixPtr* Hrep_ptr, 
+void compute_the_second_rep(dd_MatrixPtr M,
 		       dd_ErrorType* err_ptr)
 {
   dd_PolyhedraPtr poly;
-  dd_rowindex newpos;
-  dd_rowset impl_linset,redset;
-  dd_MatrixPtr Vrep, Hrep;
-
-  if (*err_ptr != dd_NoError) return;
-
+    dd_MatrixPtr A;
    /* compute the second representation */
   poly = dd_DDMatrix2Poly(M, err_ptr);
-  if (*err_ptr != dd_NoError) return;
-
-  if (*err_ptr == dd_NoError) {
-    /* compute canonical H-representation */
-    Hrep = dd_CopyInequalities(poly);
-    if (Hrep->rowsize > 0) {  /* workaround for bug with empty matrix */
-      dd_MatrixCanonicalize(&Hrep, &impl_linset, &redset, &newpos, err_ptr);
-      if (*err_ptr == dd_NoError) {
-	set_free(redset);
-	set_free(impl_linset);
-	free(newpos);
-      }
-    }
-    if (*err_ptr == dd_NoError) (*Hrep_ptr) = Hrep;
+  if (*err_ptr!=dd_NoError) goto _L99;
+    
+  switch (poly->representation) {
+    case dd_Inequality:
+        fprintf(stdout, "The second representaion:\n");
+        A=dd_CopyGenerators(poly);
+        dd_WriteMatrix(stdout,A);
+        dd_FreeMatrix(A);
+        break;
+            
+    case dd_Generator:
+        fprintf(stdout, "The second representation:\n");
+        A=dd_CopyInequalities(poly);
+        dd_WriteMatrix(stdout,A);
+        dd_FreeMatrix(A);
+        break;
+            
+    default:
+        break;
   }
 
-  if (*err_ptr == dd_NoError) {
-    /* compute canonical V-representation */
-    Vrep = dd_CopyGenerators(poly);
-    if (Vrep->rowsize > 0) {  /* workaround for bug with empty matrix */
-      dd_MatrixCanonicalize(&Vrep, &impl_linset, &redset, &newpos, err_ptr);
-      if (*err_ptr == dd_NoError) {
-	set_free(redset);
-	set_free(impl_linset);
-	free(newpos);
-      }
+  _L99:;
+ }
+
+void compute_the_second_repall(dd_MatrixPtr M,
+                            dd_ErrorType* err_ptr)
+{
+    dd_PolyhedraPtr poly;
+    dd_MatrixPtr A,B;
+    /* compute the second representation */
+    poly = dd_DDMatrix2Poly(M, err_ptr);
+    if (*err_ptr!=dd_NoError) goto _L99;
+    
+    switch (poly->representation) {
+        case dd_Inequality:
+             A=dd_CopyGenerators(poly);
+            B=dd_CopyInequalities(poly);
+            
+            fprintf(stdout, "The second representaion:\n");
+            dd_WriteMatrix(stdout,A);
+            fprintf(stdout, "\nVertex incidence\n");
+            dd_WriteIncidence(stdout,poly);
+            fprintf(stdout, "\nVertex adjacency\n");
+            dd_WriteAdjacency(stdout,poly);
+
+            fprintf(stdout, "\nThe first (input) representation\n");
+            dd_WriteMatrix(stdout,B);
+            fprintf(stdout, "\nFacet incidence\n");
+            dd_WriteInputIncidence(stdout,poly);
+            fprintf(stdout, "\nFacet adjacency\n");
+            dd_WriteInputAdjacency(stdout,poly);
+
+            dd_FreeMatrix(A);
+            dd_FreeMatrix(B);
+            break;
+            
+        case dd_Generator:
+            A=dd_CopyInequalities(poly);
+            B=dd_CopyGenerators(poly);
+            
+            fprintf(stdout, "The second representaion:\n");
+            dd_WriteMatrix(stdout,A);
+            fprintf(stdout, "\nFacet incidence\n");
+            dd_WriteIncidence(stdout,poly);
+            fprintf(stdout, "\nFacet adjacency\n");
+            dd_WriteAdjacency(stdout,poly);
+            
+            fprintf(stdout, "\nThe first (input) representation\n");
+            dd_WriteMatrix(stdout,B);
+            fprintf(stdout, "\nVertex incidence\n");
+            dd_WriteInputIncidence(stdout,poly);
+            fprintf(stdout, "\nVertex adjacency\n");
+            dd_WriteInputAdjacency(stdout,poly);
+            
+            dd_FreeMatrix(A);
+            dd_FreeMatrix(B);
+            break;
+
+        default:
+            break;
     }
-    if (*err_ptr == dd_NoError) (*Vrep_ptr) = Vrep;
-  }
-
-  dd_FreePolyhedra(poly);
+    
+_L99:;
 }
 
-
-void print_both_reps(dd_MatrixPtr Vrep, dd_MatrixPtr Hrep)
+void compute_redundancy(dd_MatrixPtr M,
+                            dd_ErrorType* err_ptr)
 {
-  /* Output V-representation */
-  dd_WriteMatrix(stdout,Vrep);
-  printf("\n");
+    dd_rowrange i,m;
+    dd_rowindex newpos;
+    dd_rowset impl_linset,redset;
+    
+    m=M->rowsize;
+    
+    fprintf(stdout, "Canonicalize the matrix.\n");
+    
+    dd_MatrixCanonicalize(&M, &impl_linset, &redset, &newpos, err_ptr);
+    
+    if (*err_ptr!=dd_NoError) goto _L99;
+    
+    fprintf(stdout, "Implicit linearity rows are:"); set_fwrite(stdout, impl_linset);
+    
+    fprintf(stdout, "\nRedundant rows are:"); set_fwrite(stdout, redset);
+    fprintf(stdout, "\n");
+    
+    fprintf(stdout, "Nonredundant representation:\n");
+    fprintf(stdout, "The new row positions are as follows (orig:new).\nEach redundant row has the new number 0.\nEach deleted duplicated row has a number nagative of the row that\nrepresents its equivalence class.\n");
+    
+    for (i=1; i<=m; i++){
+        fprintf(stdout, " %ld:%ld",i, newpos[i]);
+    }
+    fprintf(stdout, "\n");
+    dd_WriteMatrix(stdout, M);
+    
+    set_free(redset);
+    set_free(impl_linset);
+    free(newpos);
+    dd_FreeMatrix(M);
 
-  /* Output H-representation */
-  dd_WriteMatrix(stdout,Hrep);
-  printf("\n");
+_L99:;
 }
-
-
-void compute_both_reps(dd_MatrixPtr M, dd_ErrorType* err_ptr)
-{
-  dd_MatrixPtr Vrep, Hrep;
-  minimal_Vrep_Hrep(M, &Vrep, &Hrep, err_ptr);
-  if (*err_ptr != dd_NoError) return;
-
-  print_both_reps(Vrep, Hrep);
-  dd_FreeMatrix(Hrep);
-  dd_FreeMatrix(Vrep);
-}
-
-
-void compute_all(dd_MatrixPtr M, dd_ErrorType* err_ptr)
-{
-  dd_MatrixPtr Vrep, Hrep;
-  minimal_Vrep_Hrep(M, &Vrep, &Hrep, err_ptr);
-  if (*err_ptr != dd_NoError) return;
-
-  print_both_reps(Vrep, Hrep);
-  compute_adjacency(Vrep, err_ptr);
-  compute_adjacency(Hrep, err_ptr);
-  dd_FreeMatrix(Hrep);
-  dd_FreeMatrix(Vrep);
-}
-
 
 
 void usage(char *name)
@@ -165,14 +204,19 @@ void usage(char *name)
 	 "Usage:\n"
 	 "%s --option\n"
 	 "where --option is precisely one of the following:\n\n"
-	 "  --all: Compute everything.\n"
-	 "    This will compute minimal H-,V-representation and vertex and facet graph.\n"
+         "  --rep: Compute the second (H- or V-) representation.\n"
+         "         The computed representation is minimal (without redundancy).\n"
 	 "\n"
-	 "  --reps: Compute both a minimal H- and minimal V-representation.\n"
-	 "\n"
+         "  --repall: Compute the second (H- or V-) representation.\n"
+         "    It outputs both the input and output representations,\n"
+         "    as well as their incidence and adjacency relations.\n"
+         "\n"
+         "  --redcheck: Compute a minimal (non-redundant) representation.\n"
+         "              This is sometimes called the redundancy removal.\n"
+         "\n"
 	 "  --adjacency: Compute adjacency information only.\n"
 	 "    The input is assumed to be a minimal representation, as, for example, computed\n"
-	 "    by --reps. Warning, you will not get the correct answer if the input\n"
+	 "    by --redcheck. Warning, you will not get the correct answer if the input\n"
 	 "    representation is not minimal! The output is the vertex or facet graph,\n"
 	 "    depending on the input.\n"
 	 "\n"
@@ -182,23 +226,28 @@ void usage(char *name)
 }
 
 
-enum command_line_arguments { ALL, REPS, ADJACENCY };
+enum command_line_arguments {REP, REPALL, ADJACENCY, REDCHECK};
 
 
 int parse_arguments(char* arg, enum command_line_arguments* option)
 {
-  if (strcmp(arg,"--all")==0) {
-    *option = ALL;
+  if (strcmp(arg,"--repall")==0) {
+    *option = REPALL;
     return 0;
   }
-  if (strcmp(arg,"--reps")==0) {
-    *option = REPS;
+  if (strcmp(arg,"--rep")==0) {
+    *option = REP;
     return 0;
   }
   if (strcmp(arg,"--adjacency")==0) {
     *option = ADJACENCY;
     return 0;
   }
+  if (strcmp(arg,"--redcheck")==0) {
+    *option = REDCHECK;
+    return 0;
+  }
+
   printf("Unknown option: %s\n", arg);
   return 1;
 }
@@ -207,7 +256,7 @@ int parse_arguments(char* arg, enum command_line_arguments* option)
 int main(int argc, char *argv[])
 {
   dd_ErrorType err=dd_NoError;
-  dd_MatrixPtr M;
+  dd_MatrixPtr M=NULL;
   enum command_line_arguments option;
 
   if (argc!=2 || parse_arguments(argv[1],&option)) {
@@ -227,14 +276,17 @@ int main(int argc, char *argv[])
   }
 
   switch (option) {
-  case ALL:
-    compute_all(M,&err);
+  case REPALL:
+    compute_the_second_repall(M,&err);
     break;
-  case REPS:
-    compute_both_reps(M,&err);
+  case REP:
+    compute_the_second_rep(M,&err);
     break;
   case ADJACENCY:
     compute_adjacency(M,&err);
+    break;
+  case REDCHECK:
+    compute_redundancy(M,&err);
     break;
   default:
     printf("unreachable option %d\n", option);
@@ -242,7 +294,7 @@ int main(int argc, char *argv[])
   }
   
   /* cleanup */
-  dd_FreeMatrix(M);
+  if (option!=REDCHECK) dd_FreeMatrix(M); /* compute_redundancy modifies M and frees M. */
   if (err != dd_NoError) {
     dd_WriteErrorMessages(stdout,err);
   }
@@ -250,6 +302,3 @@ int main(int argc, char *argv[])
   dd_free_global_constants();
   return 0;
 }
-
-
-
